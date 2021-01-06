1160a9c7190b53aa765a224867ccdaf436bbc5b2
Greyson Christoforo <grey@christoforo.net>
2021-01-06 18:02:22 +0000

    die properly, fix build and clean up a bit

--- install-trunk.sh
+++ install-trunk.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-set -o pipefail -e
+set -e
+set -u
+set -o pipefail
 
 pacman -Syu --noprogressbar --needed --noconfirm rust
-su aurbuilder -c 'yes | yay -Syyu --noprogressbar --needed rust-wasm'
+su aurbuilder -c 'yes | yay -Syyu --removemake --noprogressbar --needed rust-wasm'
 cargo install wasm-bindgen-cli
 cargo install trunk
+
+su aurbuilder -c 'yay -Sc && rm -rf ~/.cache/yay'
