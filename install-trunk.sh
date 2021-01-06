ca09050b7c110ad43db7243ffd627ba0a6b51503
Greyson Christoforo <grey@christoforo.net>
2021-01-06 16:49:31 +0000

    rust build needs rust

--- install-trunk.sh
+++ install-trunk.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+pacman -Syu --noprogressbar --needed --noconfirm rust
 su aurbuilder -c 'yay -Syyu --noprogressbar --needed --noconfirm rust-wasm'
 cargo install wasm-bindgen-cli
 cargo install trunk
