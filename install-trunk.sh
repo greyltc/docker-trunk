b6efdb072e9761d052c43243ab526a116423c4f5
Greyson Christoforo <grey@christoforo.net>
2021-01-07 13:02:20 +0000

    install trunk from the AUR

--- install-trunk.sh
+++ install-trunk.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 pacman -Syu --noprogressbar --needed --noconfirm rust
 su aurbuilder -c 'yes | yay -Syyu --removemake --noprogressbar --needed rust-wasm'
-cargo install wasm-bindgen-cli
-cargo install trunk
+
+su aurbuilder -c 'yay -Syyu --noconfirm --removemake --noprogressbar --needed trunk'
 
 su aurbuilder -c 'yay -Sc && rm -rf ~/.cache/yay'
