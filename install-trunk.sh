3c014d623826b2b5b3d8ceafb664d0d02fa1163d
Greyson Christoforo <grey@christoforo.net>
2021-01-07 15:52:27 +0000

    fix cache clean

--- install-trunk.sh
+++ install-trunk.sh
@@ -3,9 +3,12 @@ set -e
 set -u
 set -o pipefail
 
+# build and install rust with wasm32-unknown-unknown target
 pacman -Syu --noprogressbar --needed --noconfirm rust
 su aurbuilder -c 'yes | yay -Syyu --removemake --noprogressbar --needed rust-wasm'
 
+# build and install trunk
 su aurbuilder -c 'yay -Syyu --noconfirm --removemake --noprogressbar --needed trunk'
 
-su aurbuilder -c 'yay -Sc && rm -rf ~/.cache/yay'
+# clean cache
+su aurbuilder -c 'yay -Sc --noconfirm'
