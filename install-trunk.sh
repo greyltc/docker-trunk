c8c98cf698ec8ebea90ba27364097c96439f7e8e
Greyson Christoforo <grey@christoforo.net>
2021-01-10 12:43:29 +0000

    Update install-trunk.sh

--- install-trunk.sh
+++ install-trunk.sh
@@ -7,10 +7,13 @@ set -o xtrace
 
 # build and install rust with wasm32-unknown-unknown target
 pacman -Syu --noprogressbar --needed --noconfirm rust
-sudo -u aurbuilder -D~ bash -c 'yes | yay -Syu --removemake --needed --noprogressbar rust-wasm'
+sudo -u aurbuilder -D~ bash -c "yes | yay -Syu --removemake --needed --noprogressbar rust-wasm"
+
+# clean cache
+sudo -u aurbuilder -D~ bash -c "yes | yay -Scc"
 
 # build and install trunk
-sudo -u aurbuilder -D~ bash -c 'yay -S --noconfirm --removemake --needed --noprogressbar trunk'
+sudo -u aurbuilder -D~ bash -c "yay -Syu --removemake --needed --noprogressbar --noconfirm trunk"
 
 # clean cache
 sudo -u aurbuilder -D~ bash -c 'yes | yay -Scc'
